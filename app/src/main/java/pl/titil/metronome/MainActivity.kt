8d73b266afe4df95cc1d9b1db3c427ab1963d3bb
Michal Rzeszut <rzeszut.michal@gmail.com>
2021-08-16 15:36:29 +0200

    Initial commit

---
+++ app/src/main/java/pl/titil/metronome/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.titil.metronome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
